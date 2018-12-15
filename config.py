e8f63ebed872d2fba14463bf54b91d7c7aa62c00
Fakhir Khan <thefakhir@gmail.com>
2018-12-15 21:18:45 +0500

    path correction

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-#Change the paths to the files before running this
 
 import os
 
+#Change the paths to the files before running this
+#-----------------------------------------------------------------------------
 base_path = 'D\\Tintash\\'
-movie_lines_file = os.path.join(base_path, 'movie_lines.txt')
-conversation_file = os.path.join(base_path, 'movie_conversation.txt')
+
+#-----------------------------------------------------------------------------
+# global variables depending on the values in the above module
+dataset_folder_name='cornell-movie-dialogs-corpus'
+movie_lines_file = os.path.join(base_path, dataset_folder_name, 'movie_lines.txt')
+conversation_file = os.path.join(base_path, dataset_folder_name, 'movie_conversation.txt')
